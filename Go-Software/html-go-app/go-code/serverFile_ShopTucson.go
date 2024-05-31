76f24c5d7907b91b56295f5ed7d66d51c4679549
kyle2501@gmail.com <kyle2501@gmail.com>
2024-05-30 17:52:44 -0700

    update serverFile_ShopTucson.go

---
+++ Go-Software/html-go-app/go-code/serverFile_ShopTucson.go
@@ -0,0 +1,76 @@
+// . . _ Shop Tucson - App - Website App
+
+package main
+
+// .
+
+import (
+  "os"
+  "log"
+		
+  "text/template"
+  "net/http"
+		
+  "cloud.google.com/go"
+		
+  "cloud.google.com/go/firestore"
+  "google.golang.org/api/iterator"
+)
+
+
+
+type htmlPageData struct {
+    pageTitle string
+    pagePath string
+    pageList []pageNav
+    
+}
+
+
+type pageNav struct {
+    pageTitle string
+    pageLink string
+}
+
+
+
+// . appHandler
+func appHandler(w http.ResponseWriter, r *http.Request) {
+
+    if r.URL.Path != "/app" {
+    	http.NotFound(w, r)
+    	return
+    }
+    
+// ,
+
+  pageTitle := "_ Shop Tucson - App - Website App"
+  pagePath := r.URL.Path
+  
+  pageType := ".."
+  
+  
+  pageData := htmlPageData {
+      pageTitle: pageTitle,
+      pagePath: pagePath,
+      
+      pageList: []pageNav {
+          { pageTitle: "one", pageLink: "one"},
+          { pageTitle: "two", pageLink: "two"},
+          { pageTitle: "three", pageLink: "three"},
+      },
+  	
+  }  //. .  pageData
+  
+  
+  pageFilePath := template.Must(template.ParseFiles("main_layout.html"))
+  pageFilePath.Execute(w, pageData)
+  
+}  //  .  appHandler
+
+  
+  
+  
+  
+  
+}
